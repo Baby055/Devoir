be3f7baa5a7757e4aa929416cea88a2c4133afd0
Baby055 <hei.sombiniaina@gmail.com>
2025-12-01 12:37:38 +0300

    commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String url = "jdbc:postgresql://localhost:5432/TestJDBC";
+        String user = "postgres";
+        String password = "meilleur";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connexion réussie !");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
